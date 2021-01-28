6bcf93ad87cb0c9ab7fc2e687e14b7b1f5c67e6b
LuiCat <zhtl1995@163.com>
2021-01-28 22:38:21 +0800

    Add craft table asset

---
+++ Assets/Game10/Scripts/CraftTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game01/Craft Table")]
+public class CraftTable : ScriptableObject, IDisposable {
+    public string recipe;
+    public PropBase result;
+
+    public static CraftTable Create(string recipe, PropBase result) {
+        var obj = CreateInstance<CraftTable>();
+        obj.recipe = recipe;
+        obj.result = result;
+        return obj;
+    }
+
+    public void Dispose() {
+        Destroy(this);
+    }
+}
