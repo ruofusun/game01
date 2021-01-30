2c6256aaa8bb6853c5afaa5f9f1e45d23ff55d2e
ruofusun <42979525+ruofusun@users.noreply.github.com>
2021-01-30 00:18:24 -0500

    Revert "Merge branch 'main' of https://github.com/ruofusun/game01 into main"
    This reverts commit 1ba9332846a8917cde4e47e1bcc1e04f0e6e44be, reversing
    changes made to 1c3e008d6c8c6521775bf8aab56532af0c348992.

---
+++ Assets/Game10/Scripts/Global.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+
+    public static PlayerController player;
+
+    public static HPManager hpManager;
+
+
+    public static PlayerController GetPlayer()
+    {
+        if (player == null)
+            player = FindObjectOfType<PlayerController>();
+        return player;
+    }
+
+    public static HPManager GetHpManager()
+    {
+        if (hpManager == null)
+           hpManager = FindObjectOfType<HPManager>();
+        return hpManager;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
