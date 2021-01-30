2f81ae97c5c7ada7a92b5566d4947f577a6e0499
ruofusun <42979525+ruofusun@users.noreply.github.com>
2021-01-30 08:15:09 -0500

    hitbox supports

---
+++ Assets/Game10/Scripts/Hitbox/PlayerJavelinHitbox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJavelinHitbox : PlayerHitbox
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        targetLayerMask = LayerMask.NameToLayer("Enemy");
+    }
+
+
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+      
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
