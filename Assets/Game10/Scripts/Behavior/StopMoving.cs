b7c38217f3bcbc3f1e6707637d8fa0a16138fd06
ruofusun <42979525+ruofusun@users.noreply.github.com>
2021-01-29 23:01:41 -0500

    basic behavior set up

---
+++ Assets/Game10/Scripts/Behavior/StopMoving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class StopMoving : Action
+{
+    Rigidbody2D rigidbody2D;
+    TaskStatus status = TaskStatus.Running;
+
+    public override void OnStart()
+    {
+        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = Vector2.zero;
+        status = TaskStatus.Success;
+        
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        return status;
+    }
+
+}
