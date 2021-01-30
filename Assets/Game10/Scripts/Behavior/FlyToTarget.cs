2d18420934a04215f2ffe8ec8619d24904923bf1
ruofusun <42979525+ruofusun@users.noreply.github.com>
2021-01-30 04:15:35 -0500

    pigeon supports

---
+++ Assets/Game10/Scripts/Behavior/FlyToTarget.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class FlyToTarget : Action
+{
+    public SharedVector2 target;
+    private Rigidbody2D rigidbody2D;
+
+    public float Offset = 0.25f;
+    public SharedFloat Speed = 2f;
+
+
+    public override void OnStart()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    public override TaskStatus OnUpdate()
+    {
+        if(target==null)
+        {
+            return TaskStatus.Failure;
+        }
+        var offset = target.Value - (Vector2)transform.position;
+        if(offset.sqrMagnitude<Offset )
+        {
+
+            return TaskStatus.Success;
+        }
+        rigidbody2D.velocity = offset.normalized * Speed.Value;
+
+        return TaskStatus.Running;
+
+
+    }
+
+
+}
---
+++ Assets/Game10/Scripts/Behavior/FlyToTargetWithinTimeLimit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class FlyToTargetWithTimelimit : Action
+{
+    public SharedVector2 target;
+    private Rigidbody2D rigidbody2D;
+
+    public float Offset = 0.25f;
+    public float Speed = 2f;
+
+    public float maxTime = 2f;
+    public float minTime = 1f;
+    bool timerUp = false;
+
+
+
+    public override void OnStart()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timerUp = false;
+        StartCoroutine(nameof(Timer));
+    }
+
+    IEnumerator Timer()
+    {
+        float waitTime = minTime == maxTime ? minTime : Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(waitTime);
+        timerUp = true;
+    }
+
+
+    public override TaskStatus OnUpdate()
+    {
+        if (target == null)
+        {
+            return TaskStatus.Failure;
+        }
+        var offset = target.Value - (Vector2)transform.position;
+        if (offset.sqrMagnitude < Offset||timerUp)
+        {
+            return TaskStatus.Success;
+        }
+        rigidbody2D.velocity = offset.normalized * Speed;
+
+        return TaskStatus.Running;
+
+
+    }
+
+}
