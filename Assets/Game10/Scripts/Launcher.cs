1c3e008d6c8c6521775bf8aab56532af0c348992
ruofusun <42979525+ruofusun@users.noreply.github.com>
2021-01-30 00:10:09 -0500

    javelin controller

--- Assets/Game10/Scripts/Launcher.cs
+++ Assets/Game10/Scripts/Launcher.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
+    enum itemType
+    {
+        javeline,
+        
+    }
+
+    private itemType type = itemType.javeline;
     public GameObject item;
     // Start is called before the first frame update
     void Start()
@@ -22,5 +29,9 @@ public class Launcher : MonoBehaviour
     {
         GameObject i = Instantiate(item, transform.position, Quaternion.identity);
         i.transform.RotateAround(i.transform.position, Vector3.forward, Vector2.SignedAngle(Vector2.right, dir));
+
+     //   if(type==itemType.javeline)
+         //   i.GetComponent<JavelinController>().SetDir(dir);
+        
     }
 }
