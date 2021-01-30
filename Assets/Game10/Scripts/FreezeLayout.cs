50cf4e951bbb40ed8e98c4ac9a1c7c3af140349c
LuiCat <zhtl1995@163.com>
2021-01-30 18:46:41 +0800

    qol

---
+++ Assets/Game10/Scripts/FreezeLayout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class FreezeLayout : MonoBehaviour {
+    [SerializeField] private bool isFrozen;
+    public bool IsFrozen {
+        get => isFrozen;
+        set {
+            isFrozen = value;
+            OnValidate();
+        }
+    }
+
+    private void OnValidate() {
+        foreach (var layout in GetComponentsInChildren<LayoutGroup>()) {
+            layout.enabled = isFrozen;
+        }
+        foreach (var layout in GetComponentsInChildren<ContentSizeFitter>()) {
+            layout.enabled = isFrozen;
+        }
+        foreach (var layout in GetComponentsInChildren<AspectRatioFitter>()) {
+            layout.enabled = isFrozen;
+        }
+    }
+    
+}
