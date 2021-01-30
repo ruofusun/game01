9116d38cb97ee0b2d805e801fa423b96ce9878f0
Launode <2491454629@qq.com>
2021-01-30 16:28:48 +0800

    Launode

---
+++ Assets/Game10/Scripts/MainPanelControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainPanelControl : MonoBehaviour
+{
+    public Button btn_Start;
+    public Button btn_Close;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
