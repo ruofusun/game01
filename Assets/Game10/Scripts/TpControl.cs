0c4df4f39d2f84dd63744efb4df42ae151c02660
Launode <2491454629@qq.com>
2021-01-30 17:00:41 +0800

    Update TpControl.cs

--- Assets/Game10/Scripts/TpControl.cs
+++ Assets/Game10/Scripts/TpControl.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TpControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator ani;
+    Vector3 vector3;
+    private void Start()
     {
-        
+        this.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    public void AppearTp(bool isAppear,Vector3 targetVector3) {
+        vector3 = targetVector3;
+        this.gameObject.SetActive(isAppear);
+        if (gameObject.activeInHierarchy == true)
+        {
+            ani.SetBool("IsStart", true);
+        }
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ani.SetBool("IsTp", true);
+        }
+    }
+
+    public  void ChangeScene() {
+        Global.GetPlayer().transform.position = vector3;
     }
 }
