d6077e94d8d6f9e732c1406b74c277952f841487
Launode <2491454629@qq.com>
2021-01-31 13:56:26 +0800

    Launode

---
+++ Assets/Game10/Scripts/Enemy_Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Teacher : MonoBehaviour
+{
+    Animator ani;
+    public Transform foot;
+    public bool CanCheck;
+    void Start()
+    {
+        CanCheck = false;
+        ani = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D g_left = Physics2D.Raycast(foot.position, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+        if (CanCheck)
+        {
+            if (g_left.collider)
+            {
+                ani.SetTrigger("IsGround");
+            }
+        }
+
+    }
+    void changeChecck() {
+
+        CanCheck = true;
+    }
+}
